e7594c778111bd34de367011aa5daa982effaf58
avinash4216 <77760419+avinash4216@users.noreply.github.com>
2022-08-14 10:47:23 +0530

    updated

--- DownloadZipFileAndRead.java
+++ DownloadZipFileAndRead.java
@@ -4,12 +4,14 @@ package com.java.suports;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Test;
 
 import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
@@ -54,6 +56,31 @@ public class DownloadZipFileAndRead {
         String tempDirPath = System.getProperty("java.io.tmpdir");
         System.out.println(tempDirPath);
     }
+
+    /**
+     * While split by pipe symbol we need to give as "\\|" otherwise pipe will be considered as regex
+     *
+     * split(regex) internally returns result of split(regex, 0) and in documentation of this method you can find (emphasis mine)
+     * The limit parameter controls the number of times the pattern is applied and therefore affects the length of the resulting array.
+     * If the limit n is greater than zero then the pattern will be applied at most n - 1 times, the array's length will be no greater than n, and the array's last entry will contain all input beyond the last matched delimiter.
+     * If n is non-positive then the pattern will be applied as many times as possible and the array can have any length.
+     * If n is zero then the pattern will be applied as many times as possible, the array can have any length, and trailing empty strings will be discarded.
+     *
+     * Exception:
+     * It is worth mentioning that removing trailing empty string makes sense only if such empty strings were created by the split mechanism. So for "".split(anything) since we can't split "" farther we will get as result [""] array.
+     * It happens because split didn't happen here, so "" despite being empty and trailing represents original string, not empty string which was created by splitting process.
+     */
+    @Test
+    public void splitByPipe(){
+        String row = "000267075|2008|INTERNATIONAL ASSOCIATION OF LIONS CLUBS|T|F|07-01-2008|06-30-2009|ga1960@live.com|Betty Sumner|145 Emanuel Farm Road|Lot 101|Brunswick||GA|31525|US|145 Emanuel Farm Road|Lot 101|Brunswick||GA|31520|US|||";
+        String[] strings = row.split("\\|", -1);
+        log.info("strings-size: {}", strings.length);
+        System.out.println(Arrays.deepToString(strings));
+    }
+
+
+
+
 
     @Test
     @SneakyThrows
@@ -124,7 +151,6 @@ public class DownloadZipFileAndRead {
         log.info("deleting directory: {}", fileAbsolutePath);
         FileUtils.deleteDirectory(file);
         log.info("deleted directory: {}", fileAbsolutePath);
-
     }
 
     @SneakyThrows
@@ -138,6 +164,13 @@ public class DownloadZipFileAndRead {
         rows.forEach(row -> {
             String rowString = Arrays.deepToString(row);
             System.out.println(rowString);
+        });
+    }
+
+    private void buildObject(List<String[]> rows){
+        rows.stream().filter(ArrayUtils::isNotEmpty).forEach(row -> {
+            String field1 = row[0];
+            String field2 = row[1];
         });
     }
 
@@ -160,11 +193,6 @@ public class DownloadZipFileAndRead {
         String zipFileDownloadFilePath = tempDirPath + "irs" + File.separator + "file.zip";
         log.info("zipFileDownloadFilePath: {}", zipFileDownloadFilePath);
         // File zipFile = new File(zipFileDownloadFilePath);
-
-        // download
-//        try (InputStream is = url.openStream()) {
-//            Files.copy(is, zipFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-//        }
 
         // using apache-commons-io library
         FileUtils.copyURLToFile(url, new File(zipFileDownloadFilePath));
